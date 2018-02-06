74c34a3e207ec486eafa8b814051726410230f9c
Anders Urban <minikaliffen@gmail.com>
2018-02-06 10:40:21 +0100

    Working on desing

--- eve-year-stats/src/app/templates/title-h1/title-h1.component.ts
+++ eve-year-stats/src/app/templates/title-h1/title-h1.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title-h1',
-  template: '<div class="row justify-content-md-center border-bottom pb-3 mb-4"><h1 clas="text-center" id="{{ name }}" > {{ name }} </h1></div>'
+  template: '<div class="row justify-content-md-center border-bottom pb-3 mb-4 mt-5"><h1 clas="text-center" id="{{ name }}" > {{ name }} </h1></div>'
 })
 export class TitleH1Component implements OnInit {
   @Input() name: string;
