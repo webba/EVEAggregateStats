7ebe5d0890f68e47b1eb9467cb32d422b5f8ec2e
Anders Urban <minikaliffen@gmail.com>
2018-02-08 10:57:29 +0100

    Fix some icons and added units to values

--- eve-year-stats/src/app/character-stat/character-stat.component.ts
+++ eve-year-stats/src/app/character-stat/character-stat.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class CharacterStatComponent {
 	@Input() label: string;
 	@Input() value: Object;
+	@Input() unit: Object;
 
 	constructor() { }
 
