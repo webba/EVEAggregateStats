cac566f8e248e90291dc8d9981e801c80034d123
Webba <webba93@gmail.com>
2018-02-08 14:22:24 +0000

    sidebar

---
+++ eve-year-stats/src/app/stats-navbar/stats-navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { ScrollTitle } from '../templates/title-h1/scrolltitle.interface';
+
+export class StatsNavbarPart {
+	public Title: string;
+	public Elem: ScrollTitle;
+}
+
+@Component({
+	selector: 'app-stats-navbar',
+	templateUrl: './stats-navbar.component.html',
+	styleUrls: ['./stats-navbar.component.css']
+})
+export class StatsNavbarComponent {
+	constructor() { }
+
+	@Input() parts: StatsNavbarPart[];
+
+	public scrollTo(elem: ScrollTitle): void {
+		elem.scrollTo();
+	}
+}
