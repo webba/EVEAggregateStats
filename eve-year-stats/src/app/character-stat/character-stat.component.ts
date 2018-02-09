66e4f0c761c3ac581cbd910219565c769d462634
Webba <webba93@gmail.com>
2018-02-09 00:49:45 +0000

    Some fixes

--- eve-year-stats/src/app/character-stat/character-stat.component.ts
+++ eve-year-stats/src/app/character-stat/character-stat.component.ts
@@ -9,12 +9,16 @@ import { get as _get } from 'lodash';
 export class CharacterStatComponent {
 	@Input() label: string;
 	@Input() path: string;
+	@Input() path2: string;
 	@Input() value: Object;
 	@Input() unit: string;
 
 	constructor() { }
 
 	public getValue(): number {
+		if (typeof this.path2 !== 'undefined') {
+			return _get(this.value, this.path, 0) + _get(this.value, this.path2, 0);
+		}
 		return _get(this.value, this.path, 0);
 	}
 }
