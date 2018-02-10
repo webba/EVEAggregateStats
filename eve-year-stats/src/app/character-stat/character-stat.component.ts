12268e81002a86931e02a570c17ead7db8318249
Webba <webba93@gmail.com>
2018-02-10 00:05:18 +0000

    Initial Mining

--- eve-year-stats/src/app/character-stat/character-stat.component.ts
+++ eve-year-stats/src/app/character-stat/character-stat.component.ts
@@ -9,15 +9,19 @@ import { get as _get } from 'lodash';
 export class CharacterStatComponent {
 	@Input() label: string;
 	@Input() path: string;
-	@Input() path2: string;
+	@Input() sumpath: string[];
 	@Input() value: Object;
 	@Input() unit: string;
 
 	constructor() { }
 
 	public getValue(): number {
-		if (typeof this.path2 !== 'undefined') {
-			return _get(this.value, this.path, 0) + _get(this.value, this.path2, 0);
+		if (typeof this.sumpath !== 'undefined') {
+			let aggregate = 0;
+			this.sumpath.forEach(path => {
+				aggregate += _get(this.value, path, 0);
+			});
+			return aggregate;
 		}
 		return _get(this.value, this.path, 0);
 	}
